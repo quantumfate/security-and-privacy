1aa7fae17fa36a094f0e9000ec3b11391e1e8c53
Leon Connor Holm <l.c.holm@proton.me>
2026-06-24 12:30:27 +0200

    fix: added url

--- bin/,zen-dispatcher.sh
+++ bin/,zen-dispatcher.sh
@@ -72,7 +72,7 @@ case "$URL" in
   ARGS+=("-P" "Media" "--name" "zen-twilight-media" "--no-remote")
   CONTAINER="Anime"
   ;;
-*store.steampowered.com*)
+*store.steampowered.com* | *elderscrollsonline.com*)
   ARGS+=("-P" "Media" "--name" "zen-twilight-media" "--no-remote")
   CONTAINER="Gaming"
   ;;
